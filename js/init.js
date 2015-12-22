2c06c415127804218a89361effaae3421c372f62
许智 <780272033@qq.com>
2015-12-22 09:30:17 +0800

    1

--- js/init.js
+++ js/init.js
@@ -5,3 +5,18 @@
 }).call(this);
 
 //# sourceMappingURL=init.map
+//多次点击验证
+function isTapClicks(){
+    var oo = new Object();
+    oo.t1 = (new Date()).getTime();
+    oo.fn = function(){
+        var t2  = (new Date()).getTime();
+        if(t2 - this.t1 < 200){
+            return false;
+        }else{
+            this.t1 = t2;
+            return true;
+        }
+    }
+    return oo;
+}
